335a2cc4f5eeb67cc15be746ea9e10511c157ecd
Francisco4884 <francisco.torres@epn.edu.ec>
2026-05-18 22:44:23 -0500

    mantenimiento preventivo 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { DatabaseModule } from './database/database.module';
 import { EventsModule } from './modules/events/events.module';
 import { HealthModule } from './modules/health/health.module';
 import { StatsModule } from './modules/stats/stats.module';
 
 @Module({
-  imports: [DatabaseModule, EventsModule, HealthModule, StatsModule],
+  imports: [
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 30 }]),
+    DatabaseModule,
+    EventsModule,
+    HealthModule,
+    StatsModule,
+  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
